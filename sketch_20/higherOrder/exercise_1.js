19a49941a18968dab0d48a75675fb3e3ed5f76de
emceelamb <mlam@berkeley.edu>
2017-11-14 08:37:22 -0800

    higher order practice

---
+++ sketch_20/higherOrder/exercise_1.js
@@ -0,0 +1,28 @@
+
+
+// function forEach(array, action){
+//     for(var i =0; i < array.length; i ++){
+//         action(array[i])
+//     }
+// }
+// function logEach(array){
+//     for(var i = 0; i<array.length; i++){
+//         console.log(array[i]);
+//     }
+// }
+
+// function add(array){
+//     var x = 0;
+//     for(var i =0; i<array.length;i++){
+//         x += array[i] 
+//     }
+//     return x;
+// }
+// standdard for loop
+
+var array= [1,2,3];
+// for(var i = 0; i<array.length; i++){
+//     var curr = array[i];
+//     console.log(curr);
+// }
+
