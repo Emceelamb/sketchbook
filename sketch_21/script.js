a891e8f0aabfa41ca291ba1ee0c63c67b8c6d9f6
emceelamb <mlam@berkeley.edu>
2017-11-14 21:52:18 -0800

    p5 particles

---
+++ sketch_21/script.js
@@ -0,0 +1,84 @@
+var ps =[];
+
+function preload(){
+
+}
+
+function setup() {
+    createCanvas(1280,720);
+    background(33);
+    for(var i = 0; i<20; i++){
+        ps[i] = new Particle(createVector(random(width),random(height)));
+    }
+    // p1  = new Particle(createVector(100,100));
+    // p2  = new Particle(createVector(200,100));
+}
+
+
+function draw(){
+    var f = createVector(0,0.01);
+    // p1.aforce(f);
+    // p1.update();
+    // p1.draw();
+    // p2.draw();
+    for(var i = 0; i< ps.length; i++){
+        ps[i].aforce(f);
+        ps[i].update();
+        ps[i].dead();
+        ps[i].draw();
+
+    }
+}
+
+var Particle = function(position){
+    this.position = position;
+    this.acc = createVector(0,0);
+    this.vel = createVector(random(-1,1,random(-1,1)));
+    this.grav = createVector(0,0);
+    this.mass = random(10.0);
+    this.lifespan = 255;
+    this.dead = true;
+
+    this.aforce = function(force){
+        this.force = force;
+        this.f = p5.Vector.div(this.force,this.mass);
+        this.acc.add(this.f);
+    }
+
+    this.update = function(){
+        this.vel.add(this.acc);
+        this.position.add(this.vel);
+        this.lifespan-=5;
+    }
+    
+    this.draw = function(){
+        fill(233,this.lifespan);
+        ellipse(this.position.x,this.position.y, 40,40);
+    }
+
+    this.dead = function(){
+        if(this.lifespan<0){
+            console.log("Death!");
+            ps.splice(10);
+            ps.push(new Particle(createVector(width/2,height/2)));
+            return false;
+        }
+
+
+    }
+}
+
+// class Particle {
+//     constructor(position){
+//         this.position = position;
+//     }   
+//     this.prototype.draw = function(){
+//         ellipse(this.position.x,this.position.y);
+//     }
+// }
+
+// Particle.prototype.draw = function (){
+//     console.log("hello");
+//     fill(233);
+//     ellipse(this.position.x,this.position.y, 40,40);
+// }
